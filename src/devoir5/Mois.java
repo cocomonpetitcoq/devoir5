f4bc011d5d2288189f539c808a67b622368a764b
ArnoldKamgaing <arnoldulrich237@gmail.com>
2025-04-30 13:17:29 -0400

    changement

--- src/devoir5/Mois.java
+++ src/devoir5/Mois.java
@@ -2,7 +2,7 @@ package devoir5;
 
 public enum Mois {
 
-	Janvier,Février,Mars,Avril,Mai,Juin,Juillet,Aout,Séptembre,Octobre,Novembre,Décembre;
+	Janvier,Février,Mars,Avril,Mai,Juin,Juillet,Août,Septembre,Octobre,Novembre,Décembre;
 
 
 }
