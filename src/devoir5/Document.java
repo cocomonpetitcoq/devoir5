1a8cdc4909309c66e1891d199ad0c909f8504358
ArnoldKamgaing <arnoldulrich237@gmail.com>
2025-04-30 16:05:46 -0400

    changer

--- src/devoir5/Document.java
+++ src/devoir5/Document.java
@@ -1,26 +1,26 @@
 package devoir5;
 
 public abstract class Document {
-	protected String NoEnregistrement;
-	protected String Titre;
+	private String noEnregistrement;
+	protected String titre;
 	protected int nbCopies;
 	
 	public Document( String titre, int nbCopies) {
 	
-		NoEnregistrement = générerNoEnregistrement();;
-		this.Titre = titre;
+		noEnregistrement = générerNoEnregistrement();;
+		this.titre = titre;
 		this.nbCopies = nbCopies;
 	}
 	
 	public String getNoEnregistrement() {
-		return NoEnregistrement;
+		return noEnregistrement;
 	}
 
 	public String getTitre() {
-		return Titre;
+		return titre;
 	}
 	public void setTitre(String titre) {
-		Titre = titre;
+		titre = titre;
 	}
 	public int getNbCopies() {
 		return nbCopies;
@@ -29,8 +29,8 @@ public abstract class Document {
 		this.nbCopies = nbCopies;
 	}
 
-	public String toString() {
-		return "Le No d'Enregistrement du Document est: " + NoEnregistrement + "Le titre du Document est:" + Titre + ",Le nombre de copies du Document est: " + nbCopies + "";
+	public  String toString() {
+		return "Le No d'Enregistrement du Document est: " + noEnregistrement + "Le titre du Document est:" + titre + ",Le nombre de copies du Document est: " + nbCopies + "";
 	}
 	
 	
