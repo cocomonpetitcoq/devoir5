a9c0a6e6e3f3f2c9112189ddff97c53f7fc928fb
ArnoldKamgaing <arnoldulrich237@gmail.com>
2025-04-30 16:52:51 -0400

    Document (#4)

---
+++ src/devoir5/Document.java
@@ -0,0 +1,33 @@
+package devoir5;
+
+public abstract class Document {
+	private String noEnregistrement;
+	protected String titre;
+	protected int nbCopies;
+	
+	public Document( String titre, int nbCopies) {
+	
+		noEnregistrement = générerNoEnregistrement();;
+		this.titre = titre;
+		this.nbCopies = nbCopies;
+	}
+	
+	public String getNoEnregistrement() {
+		return noEnregistrement;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public int getNbCopies() {
+		return nbCopies;
+	}
+	public void setNbCopies(int nbCopies) {
+		this.nbCopies = nbCopies;
+	}
+
+	public  abstract String toString();
+}
