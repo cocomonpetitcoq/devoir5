3cb1c862a38b462d3ca487772d2f98f3d0dcd49b
cocomonpetitcoq <122927056+cocomonpetitcoq@users.noreply.github.com>
2025-05-01 17:13:25 -0400

    validerQuantitéDocuments completed

---
+++ src/devoir5/GestionnaireDeDocuments.java
@@ -0,0 +1,16 @@
+package devoir5;
+
+public class GestionnaireDeDocuments {
+
+	public static void main(String[] args) {
+	}
+
+	private static boolean validerQuantitéDocuments() {
+		if (documents.isEmpty()) {
+			System.out.println("\nLa liste de documents est vide.");
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
