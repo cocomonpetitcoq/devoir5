a7fc321038dc8a7700331a877ef8c3e848587666
cocomonpetitcoq <122927056+cocomonpetitcoq@users.noreply.github.com>
2025-05-01 15:07:28 -0400

    Fixes

--- src/devoir5/Revue.java
+++ src/devoir5/Revue.java
@@ -4,24 +4,22 @@ public class Revue extends Document {
 	private Mois moisPublication;
 	private int annéePublication;
 
-	public Revue(String titre, int nbCopies, Mois moisPublication,int annéePublication) {	
+	public Revue(String titre, int nbCopies, Mois moisPublication, int annéePublication) {
 		super(titre, nbCopies);
 		this.moisPublication = moisPublication;
 		this.annéePublication = annéePublication;
-	
 	}
+
 	public Mois getMoisPublication() {
 		return moisPublication;
 	}
+
 	public int getAnnéePublication() {
 		return annéePublication;
 	}
 
-	
 	public String toString() {
-
-		return "Ce document est de type revue. Cette revue dont le titre est " + getTitre() + " a été publié le " + getMoisPublication() + getAnnéePublication() + " et vous avez " + getNbCopies() ;
+		return "Ce document est de type revue. Cette revue, dont le titre est " + getTitre() + ", a été publiée en "
+				+ getMoisPublication() + " " + getAnnéePublication() + ". Vous avez " + getNbCopies() + " copies.";
 	}
-	
-
 }
