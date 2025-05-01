db45e4f76f0de51cb753536993d93afa98679be5
cocomonpetitcoq <122927056+cocomonpetitcoq@users.noreply.github.com>
2025-05-01 15:45:36 -0400

    Fixes

--- src/devoir5/Roman.java
+++ src/devoir5/Roman.java
@@ -3,29 +3,32 @@ package devoir5;
 import java.util.ArrayList;
 
 public class Roman extends Livre {
-	private ArrayList<String>listesPrixLiterraire;
-	
-	public Roman(String titre, int nbCopies, String auteur, int nbPages, ArrayList<String>listesPrixLiterraire) {
-		super(titre,nbCopies,auteur,nbPages);
-		this.listesPrixLiterraire=listesPrixLiterraire;
+	private ArrayList<String> listePrixLittéraires;
+
+	public Roman(String titre, int nbCopies, String auteur, int nbPages, ArrayList<String> listePrixLittéraires) {
+		super(titre, nbCopies, auteur, nbPages);
+		this.listePrixLittéraires = listePrixLittéraires;
 	}
-	public ArrayList<String> getListesPrixLiteraire() {
-		return listesPrixLiterraire;
+
+	public ArrayList<String> getListePrixLittéraires() {
+		return listePrixLittéraires;
 	}
-	public void afficherListePrixLiterraire() {
-		if (listesPrixLiterraire.isEmpty()) {
-			System.out.println("Ce roman n'as pas gagné de prix litteraire");
-		}else {
-			System.out.println("Ce roman a gagné ");
-			for (String prix : listesPrixLiterraire) {
+
+	public void afficherListePrixLittéraires() {
+		if (listePrixLittéraires.isEmpty()) {
+			System.out.println("Ce roman n'a pas gagné de prix littéraires.");
+		} else {
+			System.out.println("Ce roman a gagné :");
+			for (String prix : listePrixLittéraires) {
 				System.out.println(prix);
 			}
 		}
 	}
+
 	public String toString() {
-		return  "Ce documneent est de type Roman. Ce Roman dont le titre est " + getTitre() + " a été écrit par " + getauteur() + " á " + getnbPages() +  " pages et vous avez "
-	+ getNbCopies() + " copies et vous avez  gagné " + getListesPrixLiteraire()+ " prix Litteraires";
+		return "Ce document est de type roman. Ce roman, dont le titre est " + getTitre() + ", a été écrit par "
+				+ getAuteur() + ", a " + getNbPages() + " pages et a gagné " + getListePrixLittéraires().size()
+				+ " prix littéraires. Vous avez " + getNbCopies() + " copies.";
 	}
-	
-	
+
 }
