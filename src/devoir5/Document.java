29485f605c85dd27598af20e6931c96c0cebe658
cocomonpetitcoq <122927056+cocomonpetitcoq@users.noreply.github.com>
2025-05-01 15:00:56 -0400

    fixed indentation

--- src/devoir5/Document.java
+++ src/devoir5/Document.java
@@ -8,19 +8,19 @@ public abstract class Document {
 	private String noEnregistrement;
 	protected String titre;
 	protected int nbCopies;
-	
-	public Document( String titre, int nbCopies) {
+
+	public Document(String titre, int nbCopies) {
 		noEnregistrement = générerNoEnregistrement();
 		this.titre = titre;
 		this.nbCopies = nbCopies;
 	}
-	
+
 	private String générerNoEnregistrement() {
 		final String CARACTÈRES = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		final int LONGUEUR = 9;
 		Random rand = new Random();
 		StringBuilder noEnregistrement = new StringBuilder();
-		
+
 		for (int i = 0; i < LONGUEUR; i++) {
 			int index = rand.nextInt(CARACTÈRES.length());
 			noEnregistrement.append(CARACTÈRES.charAt(index));
