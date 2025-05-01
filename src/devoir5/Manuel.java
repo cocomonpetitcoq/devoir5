64b5b74d09cb6c042c6122d8e5969ca8cc01d7b7
cocomonpetitcoq <122927056+cocomonpetitcoq@users.noreply.github.com>
2025-05-01 15:30:02 -0400

    Fixes

--- src/devoir5/Manuel.java
+++ src/devoir5/Manuel.java
@@ -1,16 +1,20 @@
 package devoir5;
 
-public class Manuel extends Livre{
+public class Manuel extends Livre {
 	private Domaine domaine;
+
 	public Manuel(String titre, int nbCopies, String auteur, int nbPages, Domaine domaine) {
-		super(titre, nbCopies,auteur,nbPages);
-		this.domaine=domaine;	
+		super(titre, nbCopies, auteur, nbPages);
+		this.domaine = domaine;
 	}
+
 	public Domaine getDomaine() {
 		return domaine;
 	}
-	public String toString() {
-		return  "Ce document est de type Manuel. Ce manuel dont le titre est " + getTitre()  + " a été écrit par  " + getAuteur() + " á " + getNbPages() + " pages et traite de la " + getDomaine() +" et vous avez " + getNbCopies() + " copies";
-	}
 
+	public String toString() {
+		return "Ce document est de type manuel. Ce manuel, dont le titre est " + getTitre() + ", a été écrit par "
+				+ getAuteur() + ", a " + getNbPages() + " pages et traite de la " + getDomaine() + ". Vous avez "
+				+ getNbCopies() + " copies.";
+	}
 }
