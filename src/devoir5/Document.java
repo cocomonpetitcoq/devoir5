c08da93c07a6b831b650533dd774858c4e33dc50
cocomonpetitcoq <122927056+cocomonpetitcoq@users.noreply.github.com>
2025-05-01 14:55:58 -0400

    function completed

--- src/devoir5/Document.java
+++ src/devoir5/Document.java
@@ -2,6 +2,8 @@
 
 package devoir5;
 
+import java.util.Random;
+
 public abstract class Document {
 	private String noEnregistrement;
 	protected String titre;
@@ -14,7 +16,16 @@ public abstract class Document {
 	}
 	
 	private String générerNoEnregistrement() {
+		final String CARACTÈRES = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		final int LONGUEUR = 9;
+		Random rand = new Random();
+		StringBuilder noEnregistrement = new StringBuilder();
 		
+		for (int i = 0; i < LONGUEUR; i++) {
+			int index = rand.nextInt(CARACTÈRES.length());
+			noEnregistrement.append(CARACTÈRES.charAt(index));
+		}
+		return noEnregistrement.toString();
 	}
 
 	public String getNoEnregistrement() {
