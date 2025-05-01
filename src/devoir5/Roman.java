a629930723c77efbb83e872f6752f48024e466cb
ArnoldKamgaing <arnoldulrich237@gmail.com>
2025-04-30 23:05:14 -0400

    changement

--- src/devoir5/Roman.java
+++ src/devoir5/Roman.java
@@ -3,28 +3,28 @@ package devoir5;
 import java.util.ArrayList;
 
 public class Roman extends Livre {
-	private ArrayList<String>prixLiterraire;
+	private ArrayList<String>listesPrixLiterraire;
 	
-	public Roman(String titre, int nbCopies, String auteur, int nbPages, ArrayList<String>prixLiterraire) {
+	public Roman(String titre, int nbCopies, String auteur, int nbPages, ArrayList<String>listesPrixLiterraire) {
 		super(titre,nbCopies,auteur,nbPages);
-		this.prixLiterraire=prixLiterraire;
+		this.listesPrixLiterraire=listesPrixLiterraire;
 	}
-	public ArrayList<String> getprixLiteraire() {
-		return prixLiterraire;
+	public ArrayList<String> getlistesPrixLiteraire() {
+		return listesPrixLiterraire;
 	}
 	public void afficherListePrixLiterraire() {
-		if (prixLiterraire.isEmpty()) {
-			System.out.println("Ce Roman n'as pas gagné de prix Litteraire");
+		if (listesPrixLiterraire.isEmpty()) {
+			System.out.println("Ce roman n'as pas gagné de prix litteraire");
 		}else {
 			System.out.println("Ce roman a gagné ");
-			for (String prix : prixLiterraire) {
+			for (String prix : listesPrixLiterraire) {
 				System.out.println(prix);
 			}
 		}
 	}
 	public String toString() {
 		return  "Ce documneent est de type Roman." + "Ce Roman dont le titre est " + titre  + "a été écrit par " + auteur + " á " + nbPages + " pages et vous avez"
-	+ nbCopies + "copies" + " et vous avez  gagné " + prixLiterraire + " prix Litteraires";
+	+ nbCopies + "copies" + " et vous avez  gagné " + listesPrixLiterraire + " prix Litteraires";
 	}
 	
 	
